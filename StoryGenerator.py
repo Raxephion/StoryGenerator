ad6c01c4af0376363eb60303cfbb98bcc8d709f5
Raxephion <Raxephion@gmail.com>
2022-07-07 15:01:15 +0200

    Add files via upload

---
+++ StoryGenerator.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  7 14:57:09 2022
+
+@author: Pierre-Henri Rossouw
+
+60 Apps in 60 Days Challenge - App #4: Story Generator
+
+
+"""
+import random
+when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On 20th Jan']
+who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
+name = ['Ali', 'Miriam','daniel', 'Hoouk', 'Starwalker']
+residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
+went = ['cinema', 'university','seminar', 'school', 'laundry']
+happened = ['made a lot of friends','Eats a burger', 'found a secret key', 'solved a mistery', 'wrote a book']
+print(random.choice(when) + ', ' + random.choice(who) + ' that lived in ' + random.choice(residence) + ', went to the ' + random.choice(went) + ' and ' + random.choice(happened))
